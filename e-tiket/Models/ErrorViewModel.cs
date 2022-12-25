00ac20776dbb6d29648b251c5730bc081cef0738
Joseph Nasser <bizjoz007@gmail.com>
2022-12-25 13:10:02 +0300

    Add project files.

---
+++ e-tiket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_tiket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
